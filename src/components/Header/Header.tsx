fad160d778822afd6a91d31211bd6e5b593d648b
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-24 20:07:40 +0100

    💄 additional styles added to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,22 +7,24 @@ const Header = () => {
     <div className="header__container">
       <div className="header">
         {/* social media icons */}
-        <div className="header__social">
-          <a href="/" className="social__instagram">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="/" className="social__instagram">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="/" className="social__instagram">
-            <i className="fab fa-instagram"></i>
-          </a>
-        </div>
+        <div className="header__left">
+          <div className="header__social">
+            <a href="/" className="social__instagram">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="/" className="social__instagram">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="/" className="social__instagram">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
 
-        {/* left nav */}
-        <div className="header__navLinks">
-          <a href="/">Shop</a>
-          <a href="/">Plan My Kitchen</a>
+          {/* left nav */}
+          <div className="header__navLinks">
+            <a href="/">Shop</a>
+            <a href="/">Plan My Kitchen</a>
+          </div>
         </div>
 
         {/* logo */}
@@ -30,13 +32,14 @@ const Header = () => {
           <img src="/assets/logo.svg" alt="MTK Logo" />
         </div>
 
-        {/* right nav */}
-        <div className="header__navLinks">
-          <a href="/">About</a>
-          <a href="/">Gallery</a>
+        <div className="header__right">
+          {/* right nav */}
+          <div className="header__navLinks">
+            <a href="/">About</a>
+            <a href="/">Gallery</a>
+          </div>
+          <Button type="outline" label="My Order" icon="fal fa-shopping-cart" />
         </div>
-
-        <Button type="outline" label="My Order" icon="fal fa-shopping-cart" />
       </div>
     </div>
   );
